b8170e87fd2975d9d0142f75b24f12f81cded5dc
Jerry Yang <598058395@qq.com>
2018-09-01 03:00:39 +0800

    创建后台用户管理，以及一些models和sqlite的处理

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,18 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pud_data = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question,on_delete=models.CASCADE)
+    Choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.Choice_text
